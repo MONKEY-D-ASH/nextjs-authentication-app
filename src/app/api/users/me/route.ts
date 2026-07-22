81696f7d5ac25da29046661d4478eb21ed36cdc2
aashish kumar rana <aashishkumarrana17@gmail.com>
2026-07-22 19:27:40 +0530

    add a me route and display user details on click of a button

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,6 +1,22 @@
 // grabbing the information form the token data about the user 
 
-import { getDataFromToken } from "@/helpers/getDataFromToken";
-
+import  { getDataFromToken } from "@/helpers/getDataFromToken";
 import { NextResponse, NextRequest } from "next/server";
 import User from "@/models/userModel"
+import { connect } from "@/dbConfig/dbConfig";
+
+connect()
+
+export async function GET(request: NextRequest) {
+    try {
+        const userId = await getDataFromToken(request)
+        const user = await User.findOne({_id: userId}).select("-password")
+        return NextResponse.json({
+            message: "User found",
+            data: user
+        })
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 400})
+    }
+
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,9 +2,12 @@
 import axios from "axios"
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function UserProfile() {
     const router = useRouter()
+    const [user, setUser] = useState("")
     const logout = async () => {
         try {
             const response = await axios.get("api/users/logout")
@@ -18,11 +21,18 @@ export default function UserProfile() {
         }
     }
 
+    const getUserDetails = async () => {
+        const response = await axios.get("/api/users/me")
+        console.log(response.data);
+        setUser(response.data.data._id) // because the information in the response itself is called reponse.data and the field that we require in the data field that we set
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <br />
             <p className="text-4xl">Profile page</p>
+            <h2 className="p-2 rounded bg-green-500">{user === "" ? "nothing" : <Link href={`/profile/${user}`}>{user}</Link>}</h2>
             <br/>
             <button 
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md"
@@ -30,6 +40,14 @@ export default function UserProfile() {
             >
                 Logout
             </button>
+            <br/>
+            <button 
+            className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-md"
+            onClick={getUserDetails}
+            >
+                Get User Details
+            </button>
+
         </div>
     )
 }
